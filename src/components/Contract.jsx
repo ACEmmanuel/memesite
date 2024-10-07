d54a173911f5732ed983456cbb1d1f104ffa75dc
ACEmmanuel <ahanotuemmanuel7@gmail.com>
2024-10-07 13:02:19 -0700

    ui update vs vercel error fix

--- src/components/Contract.jsx
+++ src/components/Contract.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-//Import Components & Icon
-import Clipboard from './icons/clipboard.jsx'; 
-import Check from './icons/check.jsx';
+
 
 const Contract = ({copyText, contractAddress, copied, stylee}) => {
   return (
@@ -12,7 +10,20 @@ const Contract = ({copyText, contractAddress, copied, stylee}) => {
         <p className='bg-white rounded-sm px-2 mx-auto font-perm lg:text-[18px] max-w-[15rem] md:max-w-[25rem] lg:max-w-[30rem] truncate '>{contractAddress}</p>
 
           <button onClick={copyText} className=' transition-all duration-1000 ease-in-out '>
-            {copied ? <Check  /> : <Clipboard /> }
+            {copied ? 
+            
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z" clip-rule="evenodd"/>
+            </svg>
+
+            : 
+            
+          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M18 3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1V9a4 4 0 0 0-4-4h-3a1.99 1.99 0 0 0-1 .267V5a2 2 0 0 1 2-2h7Z" clip-rule="evenodd"/>
+            <path fill-rule="evenodd" d="M8 7.054V11H4.2a2 2 0 0 1 .281-.432l2.46-2.87A2 2 0 0 1 8 7.054ZM10 7v4a2 2 0 0 1-2 2H4v6a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3Z" clip-rule="evenodd"/>
+          </svg>
+              
+            }
           </button>
         </div>
     </>
